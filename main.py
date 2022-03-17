fa593eee9ad13f682c16efef31ca8959e80282c3
Rahul M R <rahul.m.r@oracle.com>
2022-03-17 17:08:54 +0530

    tiral bg

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    version = os.getenv('namespace', default = 'ns-red')
+    return {"Message": "with Love from OCI Devops ","deployed_namespace":namespace}
