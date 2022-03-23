6b2e0dbd431f750e0cb91abcb6f63c772754fba2
Rahul M R <rahul.m.r@oracle.com>
2022-03-23 21:57:11 +0530

    tiral bg

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ app = FastAPI()
 @app.get("/v0")
 def read_root():
     version="1.1"
-    deployed_pod = os.getenv('HOSTNAME', default = 'ns-red')
-    return {"Message": "with Love from OCI Devops ","version":"version","deployed_pod":deployed_pod}
+    pod-namespace = os.getenv('pod-namespace', default = 'ns-red')
+    return {"Message": "with Love from OCI Devops ","Version":"version","Served via":pod-namespace}
